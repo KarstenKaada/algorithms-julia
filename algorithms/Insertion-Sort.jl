d957a6b5b33aa22e4121b38e94df6de37fa4b78f
Karsten Kaada <karsten.kaada@gmail.com>
2019-10-11 10:03:50 +0200

    Insertion sort fixed

--- algorithms/Insertion-Sort.jl
+++ algorithms/Insertion-Sort.jl
@@ -2,7 +2,7 @@ function insertionSort(A)
     for j = 2 : length(A)
         key = A[j]
         i = j - 1
-        while i > 0 & A[i] > key
+        while i > 0 && A[i] > key
             A[i+1] = A[i]
             i = i - 1
         end
