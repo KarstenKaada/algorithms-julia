5f4a46bb46f9d0b9bf1a7f9c9be07cbc172dcfa3
Karsten Kaada <karsten.kaada@gmail.com>
2019-10-15 08:30:25 +0200

    Minor fix in formatting

--- algorithms/Counting-Sort.jl
+++ algorithms/Counting-Sort.jl
@@ -5,12 +5,12 @@ function CountingSort(A, k)
 
 	#C[i] now contains the number of elements equal to i
 	for i = 1:length(A)
-    	C[A[i]] += 1
+	    C[A[i]] += 1
 	end
 
 	#C[i] now contains the number of elements less than or equal to i
 	for i = 2:length(C)
-    	C[i] = C[i] + C[i-1]
+	    	C[i] = C[i] + C[i-1]
 	end
 
 	for j = length(A): -1 : 1
@@ -19,7 +19,7 @@ function CountingSort(A, k)
 	end
 
 	return B
-end
+	end
 
 #test
 println(CountingSort([4,52,12,81,42,24,151,2,4,22,6,2,4,5,1,2,4],151))
